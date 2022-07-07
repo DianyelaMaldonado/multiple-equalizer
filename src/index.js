e9bd421a7f0b477d86e4beecf81746194d4f3898
Dianyela Maldonado <dianyela.y.maldonado@gmail.com>
2022-07-07 18:28:13 -0500

    added npm local funtionality, trial 0.0.1

--- src/index.js
+++ src/index.js
@@ -1,30 +1,34 @@
 const multipleEqualizer = () => {
-    const equilizerMultiContainers = document.querySelectorAll('[data-alloyEqualize-multiple]');
-    if (equilizerMultiContainers.length) {
-        equilizerMultiContainers.forEach((eContainer) => {
-            let limitItems = eContainer.dataset.alloyequalizeMultiple ? eContainer.dataset.alloyequalizeMultiple : 10;
-            for (let i = 0; i <= limitItems; i++) {
-                let equilizerItem = eContainer.querySelectorAll('[data-alloyequalize-watch-' + i + ']');
-                if (equilizerItem.length) {
-                    let itemHeightPrevious;
-                    let itemHighest = 0;
-                    equilizerItem.forEach((el) => {
-                        el.style.removeProperty('height');
-                        let itemHeight = getInnerHeight(el);
-                        if (itemHeightPrevious) {
-                            if (itemHeight > itemHighest) {
+    if(typeof document !== 'undefined'){
+        const equilizerMultiContainers = document.querySelectorAll('[data-alloyEqualize-multiple]');
+        if (equilizerMultiContainers.length) {
+            equilizerMultiContainers.forEach((eContainer) => {
+                let limitItems = eContainer.dataset.alloyequalizeMultiple ? eContainer.dataset.alloyequalizeMultiple : 10;
+                for (let i = 0; i <= limitItems; i++) {
+                    let equilizerItem = eContainer.querySelectorAll('[data-alloyequalize-watch-' + i + ']');
+                    if (equilizerItem.length) {
+                        let itemHeightPrevious;
+                        let itemHighest = 0;
+                        equilizerItem.forEach((el) => {
+                            el.style.removeProperty('height');
+                            let itemHeight = getInnerHeight(el);
+                            if (itemHeightPrevious) {
+                                if (itemHeight > itemHighest) {
+                                    itemHighest = itemHeight;
+                                }
+                                itemHeightPrevious = itemHeight;
+                            } else {
+                                itemHeightPrevious = itemHeight;
                                 itemHighest = itemHeight;
                             }
-                            itemHeightPrevious = itemHeight;
-                        } else {
-                            itemHeightPrevious = itemHeight;
-                            itemHighest = itemHeight;
-                        }
-                    });
-                    equalizeElements(equilizerItem, itemHighest);
+                        });
+                        equalizeElements(equilizerItem, itemHighest);
+                    }
                 }
-            }
-        });
+            });
+        }
+    }else {
+        console.log('Welcome');
     }
 }
 
